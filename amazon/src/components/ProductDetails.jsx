2abaea0530209459e2a375fff6e0a4ddc966ff88
timina-makena <timina.makena@student.moringaschool.com>
2025-04-24 13:42:01 +0300

    complete product details

--- amazon/src/components/ProductDetails.jsx
+++ amazon/src/components/ProductDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const ProductDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams(); 
     const [product, setProduct] = useState(null); 
     const [loading, setLoading] = useState(true); 
 
