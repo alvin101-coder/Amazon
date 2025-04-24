a29950311f4152759358b82e5ab58a7ae8235c7a
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-04-24 15:07:56 +0300

    Added a cart component

---
+++ amazon/src/components/Cart.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CartPage = ({ cart, removeFromCart }) => {
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+            {cart.length === 0 ? (
+                <p>Your cart is empty.</p>
+            ) : (
+                <div className="cart-items">
+                    {cart.map((item) => (
+                        <div key={item.id} className="cart-item">
+                            <img src={item.image} alt={item.title} />
+                            <h3>{item.title}</h3>
+                            <p>${item.price}</p>
+                            <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CartPage;
